55dd975f192befc4b18b8e342e696bd526e398d0
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-17 01:20:05 +0530

    Bottom sheet added

--- farm/lib/vegetable/tamato/tamato.dart
+++ farm/lib/vegetable/tamato/tamato.dart
@@ -1,5 +1,7 @@
-import 'package:farm/vegetable/tamato/creat.dart';
+// import 'package:farm/vegetable/tamato/creat.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Tamato extends StatefulWidget {
   const Tamato({super.key});
@@ -8,6 +10,9 @@ class Tamato extends StatefulWidget {
 }
 
 class _Tamato extends State {
+  TextEditingController custNameController = TextEditingController();
+  TextEditingController quantityController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,7 +38,6 @@ class _Tamato extends State {
         child: Padding(
           padding: const EdgeInsets.all(20),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 decoration: const BoxDecoration(
@@ -65,51 +69,187 @@ class _Tamato extends State {
               const SizedBox(
                 height: 30,
               ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const TamatoDemo(),
-                      ),
-                    );
-                  });
-                },
-                child: Center(
-                  child: Container(
-                    height: 50,
-                    width: 200,
-                    decoration: const BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Color.fromARGB(255, 126, 126, 126),
-                          blurRadius: 8,
-                          blurStyle: BlurStyle.normal,
-                          offset: Offset(10, 10),
-                        ),
-                      ],
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(5),
-                      ),
-                      color: Colors.blue,
+              Container(
+                width: 150,
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromARGB(255, 186, 185, 185),
+                      blurRadius: 5,
+                      offset: Offset(-3, 6),
                     ),
-                    child: const Center(
-                      child: Text(
-                        "Buy",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
+                  ],
+                ),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    shape: const MaterialStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
                         ),
                       ),
+                    ),
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.blue.shade300),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      bottonSheet();
+                    });
+                  },
+                  child: Text(
+                    'Buy',
+                    style: GoogleFonts.quicksand(
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black,
+                      fontSize: 23,
                     ),
                   ),
                 ),
-              ),
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+
+  void bottonSheet() {
+    showModalBottomSheet(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(50),
+        ),
+      ),
+      context: context,
+      isScrollControlled: true,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Make your order",
+                style: GoogleFonts.inter(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 30),
+              TextFormField(
+                controller: custNameController,
+                decoration: const InputDecoration(
+                  label: Text("Enter customer name"),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Color.fromRGBO(48, 11, 211, 1),
+                      width: 1.5,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                      width: 1.5,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+              TextFormField(
+                controller: quantityController,
+                decoration: const InputDecoration(
+                  label: Text("Enter quantity in KG"),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Color.fromRGBO(48, 11, 211, 1),
+                      width: 1.5,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                      width: 1.5,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+              TextFormField(
+                controller: addressController,
+                maxLines: 3,
+                decoration: const InputDecoration(
+                  label: Text("Enter your Address"),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Color.fromRGBO(48, 11, 211, 1),
+                      width: 1.5,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                      width: 1.5,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 35),
+              Container(
+                width: 200,
+                height: 50,
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromARGB(255, 186, 185, 185),
+                      blurRadius: 20,
+                      offset: Offset(-3, 6),
+                    ),
+                  ],
+                ),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.blue.shade300)),
+                  onPressed: () {},
+                  child: Text(
+                    "Buy now",
+                    style: GoogleFonts.quicksand(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30)
+            ],
+          ),
+        );
+      },
     );
   }
 }
