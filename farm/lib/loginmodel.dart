0904caea0ad16c3d14eb547d53b5c6acbc818626
flutterrushikesh <flutterrushikesh@gmail.com>
2024-03-18 00:26:11 +0530

    Retrive on first path

---
+++ farm/lib/loginmodel.dart
@@ -0,0 +1,54 @@
+class Login {
+  String name;
+  String phone;
+  String username;
+  String password;
+
+  Login({
+    required this.name,
+    required this.phone,
+    required this.username,
+    required this.password,
+  });
+
+  Map<String, String> loginMap() {
+    return {
+      'name': name,
+      'phone': phone,
+      'username': username,
+      'password': password,
+    };
+  }
+
+  @override
+  String toString() {
+    return "{name:$name, phone:$phone,username:$username, password:$password}";
+  }
+}
+
+//Tomato Model class
+class Tomato {
+  int? orderId;
+  String name;
+  String quantity;
+  String address;
+
+  Tomato(
+      {this.orderId,
+      required this.name,
+      required this.quantity,
+      required this.address});
+
+  Map<String, String> tomatoMap() {
+    return {
+      'name': name,
+      'quantity': quantity,
+      'address': address,
+    };
+  }
+
+  @override
+  String toString() {
+    return '{name:$name, quantity:$quantity, address:$address}';
+  }
+}
